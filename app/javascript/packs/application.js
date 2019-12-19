22b83d10ba3671ecf5afdd545394b4fa3d840815
Kenprogram <tadakenta@nakayukie-no-MacBook-Air.local>
2019-12-20 07:36:24 +0900

    Materialize install

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require（"jquery "）
 
+//= require materialize
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,6 +18,8 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+import '../stylesheets/application'
+import 'materialize-css/dist/js/materialize'
 import "../styles/index.scss";
 import "materialize-css/dist/js/materialize.min.js";
 M.AutoInit();
