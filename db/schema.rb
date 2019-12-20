9faf1221d80d2e4c4ebc5446edb60fca63575160
Kenprogram <tadakenta@nakayukie-no-MacBook-Air.local>
2019-12-21 07:43:50 +0900

    CRUDscaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_16_223517) do
+ActiveRecord::Schema.define(version: 2019_12_20_223942) do
+
+  create_table "recipes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
